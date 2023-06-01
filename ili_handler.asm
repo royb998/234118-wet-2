dff1c1eb49ba6d065aa36d053a84a3801cc40c99
Roy Bar-On (vm) <roy.bar@campus.technion.ac.il>
2023-06-01 20:54:26 +0000

    Working version

--- ili_handler.asm
+++ ili_handler.asm
@@ -10,34 +10,37 @@ my_ili_handler:
     pushq %r8
     pushq %rbx
     pushq %r10
-    pushq %rdi
+
     xor %rbx, %rbx
     xor %rdi, %rdi
 
-    movq 8(%rbp), %r8
-    addq 16(%rbp), %r8
-    movb (%r8), %bl
+    movq 8(%rbp), %r8		# rsi -> r8
+    movb (%r8), %bl		# ins -> bl
 
-    cmpb $0x0F, %bl
-    movq $1, %r10
+    cmpb $0x0F, %bl		# 0f == bl
+    movq $1, %r10		# 1 -> r10
     jne decide_action_ili
-    inc %r10
-    movb 1(%r8), %bl
+    inc %r10			# 2 -> r10
+    movb 1(%r8), %bl		# next ins -> bl
 
 decide_action_ili:
-    addq %rbx, %rdi
+    movq %rbx, %rdi
     call what_to_do
 
-    cmpl $0, %eax
+    cmpq $0, %rax
     jnz update_rip_ili
-    call old_ili_handler
+    popq %r10
+    popq %rbx
+    popq %r8
+    popq %rax
+    jmp *old_ili_handler
     jmp end_ili
 
 update_rip_ili:
+    movq %rax, %rdi
     addq %r10, 8(%rbp)
 
 end_ili:
-    popq %rdi
     popq %r10
     popq %rbx
     popq %r8
